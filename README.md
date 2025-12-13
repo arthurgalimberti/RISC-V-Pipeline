1a6559feb737d4468de43c208be3b5ce409304d5
arthurgalimberti <arthurgalimberti@gmail.com>
2025-12-13 16:16:25 -0300

    ta indo

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-**Para compilar:**
-iverilog -o riscv.tb riscvsingle.v
+**Para executar a simulação é necessário ter o simulator Verilator, caso não tenha em Linux:**
+sudo apt-get install verilator build-essential
 
-**Para executar:**
-vpp riscv.tb
-(finish)
+**Para converter os arquivos de Verilog para C++, execute:**
+verilator -cc --Mdir build -f riscv.f --top-module top --exe testbench.cpp
 
-**Para depurar:**
-gtkwave dump.vcd
+**Para compilar os arquivos gerados em C++:**
+make -j -C build -f Vtop.mk Vtop
+
+**Para executar a simulação:**
+./build/Vtop
--- testbench.cpp
+++ testbench.cpp
@@ -2,35 +2,37 @@
 #include "verilated.h"
 #include <iostream>
 
-vluint64_t main_time = 0;
+static vluint64_t main_time = 0;
+
+void tick(Vtop* top) {
+    top->eval();
+    main_time++;
+}
 
 int main(int argc, char** argv) {
     Verilated::commandArgs(argc, argv);
-    
+
     Vtop* top = new Vtop;
 
-    top->clk = 0;
+    top->clk   = 0;
     top->reset = 1;
 
-    while (!Verilated::gotFinish() && main_time < 1000) { // Timeout de segurança
-        
-        // Toggle do Clock
-        if ((main_time % 10) == 0) {
-            top->clk = !top->clk;
-        }
+    const vluint64_t MAX_TIME = 1000;
+    const vluint64_t RESET_RELEASE_TIME = 20;
 
-        // Tira o Reset após um tempo
-        if (main_time > 20) {
+    while (!Verilated::gotFinish() && main_time < MAX_TIME) {
+        top->clk = 0;
+        tick(top);
+
+        // Libera reset após um tempo
+        if (main_time > RESET_RELEASE_TIME) {
             top->reset = 0;
         }
 
-        // Avalia o circuito
-        top->eval();
-
-        main_time++;
+        top->clk = 1;
+        tick(top);
     }
 
-    // Limpeza
     top->final();
     delete top;
     return 0;
